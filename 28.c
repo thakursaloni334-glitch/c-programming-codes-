22912b4ff0e125c1fd22dd1995aeaff7f705a051
thakursaloni334-glitch <thakursaloni334@gmail.com>
2025-12-02 19:30:12 +0530

    Add files via upload

---
+++ 28.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the first no.");
+    scanf("%d",&a);
+    printf("Enter the second no.");
+    scanf("%d",&b);
+    if(a>b){
+    printf("%d",a);
+    }
+    else{
+    printf("%d",b);
+    }
+    return 0;
+}
